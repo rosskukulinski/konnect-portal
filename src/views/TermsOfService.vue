c1314e639130cc330634c83550f20807933c5198
Ross Kukulinski <ross@kukulinski.com>
2023-05-18 23:23:53 -0700

    feat: add nav (#2)
    * feat: add nav
    * fix: tests

---
+++ src/views/TermsOfService.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dev-portal-termsofservice">
+    <div
+      class="services-top-section flex flex-col items-center justify-center py-16 bg-section_colors-hero"
+    >
+      <h4 class="services-welcome mb-4 font-normal color-text_colors-secondary text-2xl">
+        These are the terms of services! - Hello Tuesday May 2nd.
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TermsOfService',
+  components: { },
+
+  setup () {
+    return {
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.dev-portal-services {
+  form {
+    display: flex;
+    width: 100%;
+  }
+  .services-top-section {
+    border-bottom: 1px solid var(--section_colors-stroke);
+
+    .k-input {
+      fill: var(--text_colors-accent);
+      border-radius: 3px 0 0 3px !important;
+      &::-webkit-input-placeholder { color:var(--text_colors-secondary) !important; }
+      &::-moz-placeholder { color:var(--text_colors-secondary) !important; }
+      &::-ms-placeholder { color:var(--text_colors-secondary) !important; }
+      &::placeholder { color:var(--text_colors-secondary) !important; }
+    }
+    .k-button {
+      border-radius: 0 3px 3px 0;
+      font-weight: normal !important;
+    }
+  }
+}
+</style>
